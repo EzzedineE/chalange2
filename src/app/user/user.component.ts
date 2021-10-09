d8d28b259923c6fa96a6aefa6ae43eaab58587bf
ezzedine elechi <ezzedine.elechi@gmail.com>
2021-10-09 12:09:25 +0200

    affichage tableau

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,40 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
+
+
 export class UserComponent implements OnInit {
+  resete: any;
+  Users: any[] = [];
+  tab:any[]=[]
+  aff:boolean=false;
+  submeted:boolean=false;
 
+  login = new FormGroup({  
+    nom: new FormControl('', [Validators.required]),
+    prenom: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.minLength(4), Validators.required]),
+  });
+  
+  
 
-  Users:any[]=[];
-  // login:FormGroup
-login = new FormGroup({
-      nom: new FormControl('',[Validators.required]),
-      prenom: new FormControl('',[Validators.required]),
-      email: new FormControl('',[Validators.email]),
-      password: new FormControl('',[Validators.minLength(4)]),
-
-    });
-  constructor() {
+  add() {
+     this.submeted=true
+     if(this.login.valid){
+      this.tab.push(this.login.value)
+    console.log(this.tab);
+    }
+    else{alert("enter invalide")}
     
-   }
+    }
 
-  ngOnInit(): void {
+  reset() {
+    if (this.tab.length!=0) {
+       this.aff=true
+    }
    
-  
-  }
- 
+      this.resete = {
+        'color': 'black',
+        'background-color': 'white'
+      };
 
- add() {
-   console.log(this.login.value)
-    this.Users.push(this.login.value);
-    console.log(this.Users)
-  }
- reset() {
-  
+      
+      // this.login.value.non=""
+      // this.login.value.prenom=""
+      // this.login.value.email=""
+      // this.login.value.password=""
+  this.login.reset()
 }
 
+supprimer(i:number){
+  this.tab.splice(i,1)
 }
+// *******************************************************************************************
+  constructor() {}
+  ngOnInit(): void { }
+}
+
+
+
+
