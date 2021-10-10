45fe5744404f9fab1570ef8489b5c7a4297e5ab8
ezzedine elechi <ezzedine.elechi@gmail.com>
2021-10-10 14:40:05 +0200

    validation password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserComponent
-  ],
+  declarations: [AppComponent, UserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,65 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { PasswordValidator } from '../password-validateur';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+
   styleUrls: ['./user.component.css'],
 })
-
-
 export class UserComponent implements OnInit {
   resete: any;
   Users: any[] = [];
-  tab:any[]=[]
-  aff:boolean=false;
-  submeted:boolean=false;
-
-  login = new FormGroup({  
-    nom: new FormControl('', [Validators.required]),
-    prenom: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.minLength(4), Validators.required]),
-  });
-  
-  
+  tab: any[] = [];
+  aff: boolean = false;
+  submeted: boolean = false;
+  login = new FormGroup(
+    {
+      nom: new FormControl('', [Validators.required]),
+      prenom: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.minLength(4),
+        Validators.required,
+      ]),
+      password2: new FormControl('', [
+        Validators.minLength(4),
+        Validators.required,
+      ]),
+    },
+    { validators: [PasswordValidator] }
+  );
 
   add() {
-     this.submeted=true
-     if(this.login.valid){
-      this.tab.push(this.login.value)
-    console.log(this.tab);
+    this.submeted = true;
+    if (this.login.valid) {
+      this.tab.push(this.login.value);
+    } else {
+      alert('enter invalide');
     }
-    else{alert("enter invalide")}
-    
-    }
+  }
 
   reset() {
-    if (this.tab.length!=0) {
-       this.aff=true
+    if (this.tab.length != 0) {
+      this.aff = true;
     }
-   
-      this.resete = {
-        'color': 'black',
-        'background-color': 'white'
-      };
+    this.resete = {
+      color: 'black',
+      'background-color': 'white',
+    };
+    this.login.reset();
+  }
 
-      
-      // this.login.value.non=""
-      // this.login.value.prenom=""
-      // this.login.value.email=""
-      // this.login.value.password=""
-  this.login.reset()
-}
+  supprimer(i: number) {
+    confirm;
+    this.tab.splice(i, 1);
+  }
 
-supprimer(i:number){
-  this.tab.splice(i,1)
-}
-// *******************************************************************************************
+  isSupp(i: number) {
+    if (this.tab[i].password.length < 10) {
+      return true;
+    }
+    return false;
+  }
+
   constructor() {}
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
-
-
-
-
